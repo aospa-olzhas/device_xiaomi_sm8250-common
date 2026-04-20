871d8f9bffe4b2c6d948028c9fa29fe36cd39d15
kaffuchino233 <qkivan@proton.me>
2026-04-20 11:19:51 +0800

    munch: Satisfy FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,6 +162,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    device/xiaomi/sm8250-common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 ODM_MANIFEST_SKUS += nfc
