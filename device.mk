c1409a0151e4a71c63a67cf30fd176157e0c99c5
kaffuchino233 <qkivan@proton.me>
2026-05-29 17:24:14 +0800

    Revert "sm8250-common: Switch to common lights HAL"
    This reverts commit c3749edcc65724f23cf70c2f804634883442f06f.

--- device.mk
+++ device.mk
@@ -286,7 +286,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.lineage
+    android.hardware.light-service.xiaomi
 
 # Lineage Health
 PRODUCT_PACKAGES += \
